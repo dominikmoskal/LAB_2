050adf8387dcc6ac2fd7f6ec816b37af37dba560
dominikmoskal <100564913+dominikmoskal@users.noreply.github.com>
2022-04-24 20:50:38 +0200

    Update Main.java

--- LAB4/src/com/company/Main.java
+++ LAB4/src/com/company/Main.java
@@ -1,46 +1,52 @@
 package com.company;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
-
-    public static void zadanie1(){
-        double N;
-        int a;
-        int b;
-        int ileN = 0;
-        int ileAB = 0;
-        int ileAN = 0;
-        Scanner scan = new Scanner(System.in);
-        N = scan.nextInt();
-        a = scan.nextInt();
-        b = scan.nextInt();
-        for (int i = 0; i < N; i++) {
-            if(i%3==0) ileN++;
-        }
-        for (int i = a; i <=b ; i++) {
-            if(i%3==0) ileAB++;
-        }
-        if(a<N && b>N) {
-            for (int i = a; i <= N; i++)
-                if (i % 3 == 0) ileAN++;
-            System.out.println("Liczba podizelnych przez 3 w przedziale od "+a+" do "+N+" wynosi "ileAN);
-        }
-        if(a<N && b<N)
-            System.out.println("Liczb podzielnych przez 3 z przedzialu od "+a+" do "+b+"jest "+ileAB);
-        if(a>N)
-            System.out.println("Brak liczb podzielnych przez 3 zawierających w przedziale");
-
-        System.out.println("Liczba podizelnych przez 3 w przedziale N: "+ileN);
-        System.out.println("Liczba podizelnych przez 3 w przedziale ab: "+ileAB);
-    }
-
-
-
     public static void main(String[] args) {
-
-
+        Zadanie_1();
+        System.out.println();
+        Zadanie_2();
+        System.out.println();
+        Zadanie_3();
+    }
+    public static void Zadanie_1() {
+        double suma = 0;
+        int[] tab1 = {1, 2, 3, 4, 5, 6};
+        for (int i = 0; i < tab1.length; i++) {
+            suma = suma + tab1[i];
+        }
+        System.out.println("Suma: " + suma);
+        System.out.println("Średnia: " + suma / tab1.length);
+    }
+    public static void Zadanie_2() {
+        boolean[] tab1 = new boolean[20];
+        for (int i = 0; i < tab1.length; i++) {
+            if (i==0 || i % 2 == 0) System.out.println(tab1[i] = true);
+            else System.out.println(tab1[i] = false);
+        }
+    }
+    public static double Losuj() {
+        Random rand = new Random();
+        double liczba = rand.nextInt(45) - 10;
+        return liczba;
+    }
+    public static void Zadanie_3() {
+        double[] array = new double[20];
+        double suma = 0, sumaRoznic = 0, srednia;
+        System.out.println("Elementy tablicy: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Losuj();
+            suma += array[i];
+            System.out.print(array[i] + "\t");
+        }
+        srednia = suma / (double) array.length;
+        System.out.println("\nSuma: " + suma + "\nŚrednia pomiarów: " + srednia);
+        for (int i = 0; i < array.length; i++) {
+            sumaRoznic += Math.pow(array[i] - srednia, 2.0);
+        }
+        System.out.println("Suma różnic: " + sumaRoznic);
+        System.out.println("Wariancja: " + sumaRoznic / (double) ((array.length) - 1));
     }
 }
